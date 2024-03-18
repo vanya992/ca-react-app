d2e6900122bbf3720c8cd1efb39ecf489afda7e6
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-03-18 14:26:32 +0100

    Combined layout of front page made

---
+++ src/Components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Header";
+import Footer from "../Footer";
+
+function Layout(props) {
+    return (
+        <>
+        <Navbar />
+        <Footer />
+        </>
+    )
+}
+
+export default Layout;
