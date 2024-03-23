50b12d199df10baac04e61e494e19e49beacdeb7
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-03-23 23:54:07 +0100

    changed positions of routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Home } from "./Pages/Home";
 import Layout from "./Components/Layout";
 import "./style.css";
 
 function App() {
-  return <Layout />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<div>Products</div>} />
+          <Route path="contact" element={<div>Contact</div>} />
+          <Route path="cart" element={<div>Cart</div>} />
+          <Route path="*" element={<div>Route not found</div>} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
-
