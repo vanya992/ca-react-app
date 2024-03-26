59e1a8426a2c747be39b621a1a72a7ae52037fba
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-03-26 09:33:56 +0100

    Installed react helmet, zustand and react-router-dom

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,55 +3,47 @@ import logo from "../../assets/images/logo.png";
 import shoppingCart from "../../assets/images/shoppingCart.png";
 import search from "../../assets/images/search.png";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Home } from "../../Pages/Home";
 import styles from "./Header.module.css";
 
 function Navbar() {
   return (
-    <BrowserRouter>
-      <nav className={styles.nav}>
-        <Link to="/">
-          <img
-            src={logo}
-            alt="Logo of the market place"
-            className={styles.logoImg}
-          />
-        </Link>
+    <nav className={styles.nav}>
+      <Link to="/">
+        <img
+          src={logo}
+          alt="Logo of the market place"
+          className={styles.logoImg}
+        />
+      </Link>
 
-        <div className={styles.searchSection}>
-          <input type="text" placeholder="Search" />
-          <img
-            src={search}
-            alt="magnifying glass icon"
-            className={styles.searchIcon}
-          />
-        </div>
+      <div className={styles.searchSection}>
+        <input type="text" placeholder="Search" />
+        <img
+          src={search}
+          alt="magnifying glass icon"
+          className={styles.searchIcon}
+        />
+      </div>
 
-        <ul>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/cart">
-              <img
-                src={shoppingCart}
-                alt="Logo of the market place"
-                className={styles.shoppingCart}
-              />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route index element={<div>Home</div>} />
-        <Route path="products" element={<div>Products</div>} />
-        <Route path="contact" element={<div>Contact</div>} />
-        <Route path="cart" element={<div>Cart</div>} />
-        <Route path="*" element={<div>Route not found</div>} />
-      </Routes>
-    </BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            <img
+              src={shoppingCart}
+              alt="Logo of the market place"
+              className={styles.shoppingCart}
+            />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
