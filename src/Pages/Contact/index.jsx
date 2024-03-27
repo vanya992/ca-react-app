391ab5a317534fb086d51989b8dbf32ffcc3b3dd
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-03-27 08:59:08 +0100

    Contact page added content

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import styles from "./Contact.module.css";
+import logo2 from "../../assets/images/logo2.png";
 
 export const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -35,48 +36,65 @@ export const ContactForm = () => {
       <Helmet>
         <title>Contact | Market.</title>
       </Helmet>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <label>Name: </label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-          minLength="3"
-        />
+      <div class={styles.container}>
+        <section className={styles.sectionOne}>
+          <img src={logo2} alt="Contact page" />
+          <h1>Contact Us</h1>
+          <h2>Welcome to Market. Customer Support</h2>
+          <p>
+            At Market., we're committed to offering you a seamless shopping
+            experience across our wide range of products. Whether you have
+            questions, need assistance with your order, or want to provide
+            feedback, we're here to help.
+          </p>
+        </section>
+        <section className={styles.sectionTwo}>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <label>Name: </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              minLength="3"
+            />
 
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          minLength="3"
-        />
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              minLength="3"
+            />
 
-        <label>Subject</label>
-        <input
-          type="text"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-          required
-          minLength="3"
-        />
+            <label>Subject</label>
+            <input
+              type="text"
+              name="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+              minLength="3"
+            />
 
-        <label>Message:</label>
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-          minLength="3"
-        />
+            <label>Message:</label>
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              minLength="3"
+            />
 
-        <button type="submit">Send</button>
-      </form>
+            <button type="submit" className="send">
+              Send
+            </button>
+          </form>
+        </section>
+      </div>
     </main>
   );
 
