5d6b95349b7107379caa00cf7ce23a05171cc21a
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-04-05 15:10:12 +0200

    cart and checkout added for export

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -2,3 +2,5 @@ export { Home } from "./Home/index";
 export { Products } from "./Products/index";
 export { ContactForm } from "./Contact/index";
 export { Product } from "./Product/index";
+export { Cart } from "./Cart/index";
+export { Checkout } from "./Checkout/index";
