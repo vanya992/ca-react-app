e3a5ff06908d28a0593efbef4befb3c095032b48
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-04-05 15:09:50 +0200

    Cart and cart product adjustments in styling and displaying

--- src/Components/CartProduct/index.jsx
+++ src/Components/CartProduct/index.jsx
@@ -1,40 +1,57 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useCartStore } from "../../store/cartStore";
-import styles from "./CartProduct.module.jsx";
+import styles from "./CartProduct.module.css";
+import { AmountHandler } from "../AmountHandler";
 
 export const CartProduct = ({ product, onCountChange }) => {
-  const [price, setPrice] = useState(Math.min(product.price, product.discountedPrice));
+  const [price, setPrice] = useState(
+    Math.min(product.price, product.discountedPrice)
+  );
 
   const isDiscounted = product.price > product.discountedPrice;
-  const priceClassNames = isDiscounted ? `${styles.price} ${styles.strikethrough}` : styles.price;
-  const discountClassNames = isDiscounted ? styles.discountedPrice : '';
-  const percentage = isDiscounted ? `${(((product.price - product.discountedPrice) / product.price) * 100).toFixed(0)}%` : null;
+  const priceClassNames = isDiscounted
+    ? `${styles.price} ${styles.strikethrough}`
+    : styles.price;
+  const discountClassNames = isDiscounted ? styles.discountedPrice : "";
+  const percentage = isDiscounted
+    ? `${(
+        ((product.price - product.discountedPrice) / product.price) *
+        100
+      ).toFixed(0)}%`
+    : null;
 
   const handleCountChange = (newCount) => {
-    const updatedPrice = Math.min(product.price, product.discountedPrice) * Math.max(newCount, 0);
+    const updatedPrice =
+      Math.min(product.price, product.discountedPrice) * Math.max(newCount, 0);
     setPrice(updatedPrice);
-    onCountChange(updatedPrice, Math.max(newCount, 0));
+    onCountChange(updatedPrice, Math.max(newCount, 0), product.id);
   };
 
   return (
-    <div>
+    <div className={styles.cartProduct}>
       <Link to={`/product/${product.id}`}>
-        <img src={product.imageUrl} alt={product.title} />
+        <img
+          src={product.image.url}
+          alt={product.title}
+          className={styles.productImage}
+        />
       </Link>
-      <div>
-        <h1>{product.title}</h1>
-        <div>
-          {!isDiscounted && `NOK ${price.toFixed(2)}`}
-          <div className={priceClassNames}>
-            {isDiscounted && `NOK ${price.toFixed(2)}`}
-          </div>
-          <div className={styles.discountPriceContainer}>
-            {isDiscounted && (
-              <div className={discountClassNames}>- {percentage}</div>
-            )}
-            {isDiscounted && `NOK ${price}`}
-          </div>
+      <div className={styles.productDetails}>
+        <div className={styles.productPricing}>
+          <h3>{product.title}</h3>
+          {isDiscounted && (
+            <div className={`${styles.price} ${styles.strikethrough}`}>
+              NOK {product.price.toFixed(2)}
+            </div>
+          )}
+          <div className={discountClassNames}>NOK {price.toFixed(2)}</div>
+          {isDiscounted && <div>- {percentage} off</div>}
+          <AmountHandler
+            plusCount={1}
+            minusCount={1}
+            onCountChange={handleCountChange}
+            cartItem={product}
+          />
         </div>
       </div>
     </div>
---
+++ src/Pages/Cart/index.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import styles from "./Cart.module.css";
+import { useCartStore } from "../../store/cartStore";
+import { CartProduct } from "../../Components/CartProduct";
+import { AlertNotification } from "../../Components/AlertNotification";
+import { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { Link, useNavigate } from "react-router-dom";
+
+export const Cart = () => {
+  const products = useCartStore((state) => state.products);
+  const clearProducts = useCartStore((state) => state.clearProducts);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const navigate = useNavigate();
+  const isCartEmpty = products.length === 0;
+
+  useEffect(() => {
+    calculateTotalPrice();
+  }, [products]);
+
+  const calculateTotalPrice = () => {
+    let total = 0;
+    products.forEach((product) => {
+      const price = Math.min(product.price, product.discountedPrice);
+      total += price * (product.count || 1);
+    });
+    setTotalPrice(total);
+  };
+
+  const handleCountChange = (updatedPrice, newCount, productId) => {
+    useCartStore.getState().updateProductCount(productId, newCount);
+    calculateTotalPrice();
+  };
+
+  const [showLabel, setShowLabel] = useState(false);
+
+  const handleUnmount = () => {
+    setShowLabel(false);
+  };
+
+  const handleCheckout = (e) => {
+    e.preventDefault();
+    if (products.length === 0) {
+      setShowLabel(true);
+    } else {
+      clearProducts();
+      navigate("/checkout");
+    }
+  };
+
+  return (
+    <main>
+      <Helmet>
+        <title>Cart | Market.</title>
+      </Helmet>
+      {showLabel && (
+        <AlertNotification
+          message={"You have no items in your cart."}
+          onUnmount={handleUnmount}
+        />
+      )}
+      <section>
+        <h1>Cart</h1>
+        {products.length === 0 ? (
+          <>
+            <p>Your cart is empty. Go back and find something you like.</p>
+            <Link to="/products" className="cartButton">
+              Back to shopping
+            </Link>
+          </>
+        ) : (
+          products.map((product) => (
+            <CartProduct
+              product={product}
+              key={product.id}
+              onCountChange={(updatedPrice, newCount) =>
+                handleCountChange(updatedPrice, newCount, product.id)
+              }
+            />
+          ))
+        )}
+        <div className={styles.totalAndCheckout}>
+          {!isCartEmpty && (
+            <>
+              <div>NOK {totalPrice.toFixed(2)}</div>
+              <Link
+                to="/checkout"
+                onClick={handleCheckout}
+                className="ctaButton"
+              >
+                Checkout
+              </Link>
+            </>
+          )}
+        </div>
+      </section>
+    </main>
+  );
+};
