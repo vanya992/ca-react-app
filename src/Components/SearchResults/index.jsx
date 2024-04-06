7dc518d9a80a5bc2e565844e62389122c5275ef2
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-04-06 20:28:18 +0200

    no results for searchmessage

--- src/Components/SearchResults/index.jsx
+++ src/Components/SearchResults/index.jsx
@@ -3,6 +3,12 @@ import styles from "./SearchResults.module.css";
 import { Link } from "react-router-dom";
 
 export const SearchResults = ({ results }) => {
+  if (!results.title) {
+    return (
+      <div className={styles.noResults}>No product matches your search.</div>
+    );
+  }
+
   return (
     <div className={styles.results}>
       {results.map((result, id) => (
