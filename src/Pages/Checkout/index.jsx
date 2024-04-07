d9351e5be5293e925021fe10c47bd7404a9913e7
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-04-07 21:12:49 +0200

    updated

--- src/Pages/Checkout/index.jsx
+++ src/Pages/Checkout/index.jsx
@@ -12,9 +12,10 @@ export const Checkout = () => {
         <img src={logo} alt="logo" />
         <h2>Checkout successful!</h2>
         <p>
-          Thank you so much for your order. We appreciate your trust and hope
-          you will be satisfied with your purchase. You will receive an order
-          confirmation to the email you provided with your order shortly.
+          Thank you for your order. We appreciate your trust and hope you will
+          be satisfied with your purchase. You will receive an order
+          confirmation to the email you provided with your order shortly. For
+          any further information feel free to contact us.
         </p>
         <p>You can track your order live with order number: #1206at </p>
         <div></div>
